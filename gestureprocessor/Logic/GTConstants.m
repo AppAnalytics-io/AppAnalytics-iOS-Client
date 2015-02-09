5b8e3f02afe70a1c643fc6492b9760d2abea8154
hasancc <hasancanyasar@gmail.com>
2015-02-09 20:02:36 +0200

    changed API base url

--- gestureprocessor/Logic/GTConstants.m
+++ gestureprocessor/Logic/GTConstants.m
@@ -8,7 +8,7 @@ size_t const kGTMaxSamplesSizeInBytes   = 1024 * 100;
 float const kGTSerializationInterval    = 15.0f;
 float const kGTSendingDataInterval      = 60.0f;
 
-NSString* const kGTBaseURL      = @"http://www.appanalytics.io/api/v1"; // @"http://192.168.1.36:6249/api/v1";
+NSString* const kGTBaseURL      = @"http://wa-api-cent-us-1.cloudapp.net/api/v1";
 NSString* const kGTManifestsURL = @"manifests";
 NSString* const kGTSamplesURL   = @"samples";
 
