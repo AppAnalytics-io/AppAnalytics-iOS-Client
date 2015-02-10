ded1909bb845b79f9edd86bd68c3d3333b268512
V8tr <vadim@tundramobile.com>
2015-02-10 12:21:21 +0200

    Omitting UIInputWindowController class while tracking navigation.

--- gestureprocessor/Logic/UIViewController+Tracking.m
+++ gestureprocessor/Logic/UIViewController+Tracking.m
@@ -2,63 +2,27 @@
 #import "NSObject+Swizzling.h"
 #import <objc/runtime.h>
 #import "Logger.h"
-#import <Foundation/Foundation.h>
 
 @implementation UIViewController (Tracking)
-
-static NSMutableSet* gHashes;
-static NSUInteger const kMaxHashPool = 16;
 
 + (void)load
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^
     {
-        [self swizzleOriginalMethod:@selector(viewDidLoad)
-                               with:@selector(viewDidLoadSwizzled)];
-        
         [self swizzleOriginalMethod:@selector(viewWillAppear:)
                                with:@selector(viewWillAppearSwizzled:)];
-        
-        gHashes = [NSMutableSet set];
     });
-}
-
-- (void)viewDidLoadSwizzled
-{
-    [self viewDidLoadSwizzled];
-    if (addObjectHash([self hash]))
-    {
-        [[Logger instance] navigationRecognizedWithViewControllerID:NSStringFromClass(self.class)];
-    }
 }
 
 - (void)viewWillAppearSwizzled:(BOOL)animated
 {
     [self viewWillAppearSwizzled:animated];
-    if (addObjectHash([self hash]))
+    static NSString* omittedClass = @"UIInputWindowController";
+    NSString* className = NSStringFromClass(self.class);
+    if (![className isEqualToString:omittedClass])
     {
         [[Logger instance] navigationRecognizedWithViewControllerID:NSStringFromClass(self.class)];
-    }
-}
-
-static inline BOOL addObjectHash(NSUInteger hash)
-{
-    if (gHashes.count > kMaxHashPool)
-    {
-        [gHashes removeAllObjects];
-    }
-    
-    id hashObject = @(hash);
-    
-    if ([gHashes containsObject:hashObject])
-    {
-        return NO;
-    }
-    else
-    {
-        [gHashes addObject:hashObject];
-        return YES;
     }
 }
 
